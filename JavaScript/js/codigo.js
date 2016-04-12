82b8105b3851bc4d692cec1ffcd021c5d2330ccb
karenruiz <pkarenruiz@gmail.com>
2016-04-12 11:01:36 -0600

    Archivo JS, funcion anonima y consola

---
+++ JavaScript/js/codigo.js
@@ -0,0 +1,13 @@
+var botonClic = function()
+{
+	alert("Me diste click otra vez :$");
+}
+
+var nombre = "Karen"
+window.document.write("<strong>JS en acción</strong>"+" "+nombre);
+console.log("A punto de lanzar un mensaje.")
+alert("Mensaje por pantalla");
+
+
+// En la consola del navegador si le pones el nombre de la variable sale lo que contiene
+// y si le pones la variable y después "()" se ejecuta lo que contiene.
