8d0a54bd1858dbda2d97acb60ec5d205a3958c81
karenruiz <pkarenruiz@gmail.com>
2016-05-11 11:00:54 -0600

    Continuacion de Jquery2

--- jquery2/js/eventos.js
+++ jquery2/js/eventos.js
@@ -2,12 +2,14 @@ var iniciaApp =function()
 {
 	var validaEntrada = function()
 	{
+		//Invalida los eventos que no corresponden a esta función.
+		event.preventDefault();
 		var usuario	=$("#txtUsuario").val();
 		var clave	=$("#txtClave").val();
 		//Validaciones
 
 			//1-.Que no sean vacios
-			if(usuarios =="")
+			if(usuario =="") 
 			{
 				alert("El usuario no debe ser vacío")
 				("#txtUsuario").focus();
@@ -18,6 +20,19 @@ var iniciaApp =function()
 				alert("La clave no debe ser vacía")
 				("#txtClave").focus();
 			}
+			//2.-Verificar usuario y contraseña
+			if(usuario=="pw" && clave=="1234")
+			{
+				alert("Bienvenido "+usuario);
+				//Dar entrada al usuario
+				$("#datosUsuario").hide();//escondemos
+				$("nav").show("slow");//mostramos
+			}
+			else
+				alert("Usuario y/o contraseña incorrecta(s)");
+
+
+
 		console.log("Se disparó el submit :(")
 	}
 	$("#frmValidaEntrada").on("submit",validaEntrada)
